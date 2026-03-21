f3d6ba60b5fa7994c68ad23049613f08c105a7b5
Jonathan5440 <jvaldezd4@miumg.edu.gt>
2026-03-20 21:15:58 -0600

    Mejora dashboard agrega ultimo alumno registrado

--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
 from universidad.Models.Alumno.models import Alumno
+
 def dashboard(request):
     context = {
-        'total_alumnos'   : Alumno.objects.count(),
-        'activos'         : Alumno.objects.filter(is_active=True).count(),
-        'inactivos'       : Alumno.objects.filter(is_active=False).count(),
+        'total_alumnos' : Alumno.objects.count(),
+        'activos'       : Alumno.objects.filter(is_active=True).count(),
+        'inactivos'     : Alumno.objects.filter(is_active=False).count(),
+        'ultimo_alumno' : Alumno.objects.order_by('-enrolled_at').first(),
     }
     return render(request, 'core/dashboard.html', context)
